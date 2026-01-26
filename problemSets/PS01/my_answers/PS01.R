b91cba16202df146689409e5395edf225b18bf08
kajkowsc <kajkowsc@tcd.ie>
2026-01-26 17:20:55 +0000

    Continued Work on PS01

--- problemSets/PS01/my_answers/PS01.R
+++ problemSets/PS01/my_answers/PS01.R
@@ -44,3 +44,45 @@ names(MEP_data)[names(MEP_data) == "MEP id"]  <- "MEPID"
 #Combining the two data sets
 combined_data <- merge(MEP_data, EP1_data)
 
+#Mean yes rates for each EP group
+yes_means <- combined_data %>%
+  group_by(EPG) %>%
+  summarise(
+    n_yes = sum(vote == "Yes", na.rm = TRUE),
+    n_no = sum(vote == "No", na.rm = TRUE),
+    n_abstain = sum(vote == "Abstain", na.rm = TRUE),
+    mean_yes_rate = n_yes / (n_yes + n_no + n_abstain),
+    .groups = "drop"
+  )
+print(yes_means)
+
+#Mean abstention rates for each EP Group
+abstain_means <- combined_data %>%
+  group_by(EPG) %>%
+  summarise(
+    n_yes = sum(vote == "Yes", na.rm = TRUE),
+    n_no = sum(vote == "No", na.rm = TRUE),
+    n_abstain = sum(vote == "Abstain", na.rm = TRUE),
+    mean_abstain_rate = n_abstain / (n_yes + n_no + n_abstain),
+    .groups = "drop"
+  )
+print(abstain_means)
+
+#Mean vote preferences along the two contested dimensions
+#When I first ran the code, a warning came up because the 
+#the values were characters, so I converted them to numeric
+combined_data <- combined_data %>%
+  mutate(
+    NOM_D1 = as.numeric(as.character(`NOM-D1`)),
+    NOM_D2 = as.numeric(as.character(`NOM-D2`))
+  )
+
+NOM_group_means <- combined_data %>%
+  group_by(EPG) %>%
+  summarise(
+    mean_nomd1 = mean(`NOM-D1`, na.rm = TRUE),
+    mean_nom2 = mean(`NOM-D2`, na.rm),
+    n_MEPS = n(),
+    .groups = "drop"
+  )
+    
